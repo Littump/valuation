89ad7e3f9d03eb5fc2bd07bf4ba2263b53904968
Roman Yakimov <littump@yandex.ru>
2023-10-16 22:47:38 +0300

    add localfile -> pil.image

--- backend/api/ml/utils.py
+++ backend/api/ml/utils.py
@@ -1,6 +1,20 @@
+from PIL import Image
+import io
+
+
+def convert_uploaded_files_to_images(uploaded_files):
+    images = []
+    for uploaded_file in uploaded_files:
+        file_data = uploaded_file.read()
+        image = Image.open(io.BytesIO(file_data))
+        images.append(image)
+    return images
+
+
 def calculate_price(data):
     return None
 
 
 def get_repair(photos):
+    photos = convert_uploaded_files_to_images(photos)
     return None
