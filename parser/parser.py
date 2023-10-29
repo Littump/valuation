59125a1ff022b790907551491fdfc7b2b169fe3c
Roman Yakimov <littump@yandex.ru>
2023-10-29 19:48:03 +0300

    parser done

--- parser/parser.py
+++ parser/parser.py
@@ -5,6 +5,7 @@ driver = webdriver.Chrome()
 
 max_str = 54
 
+
 def create_table():
     ...
 
@@ -12,11 +13,14 @@ def create_table():
 def insert_data():
     ...
 
+
 def parse(url_params):
     url = 'https://www.cian.ru/cat.php?deal_type=sale&engine_version=2'
     for page in range(1, max_str + 1):
         url_params['p'] = page
-        url_with_params = url + "&" + "&".join([f"{key}={value}" for key, value in url_params.items()])
+        url_with_params = url + "&" + "&".join(
+            [f"{key}={value}" for key, value in url_params.items()]
+        )
         driver.get(url_with_params)
         current_url = driver.current_url
         parsed_url = urlparse(current_url)
@@ -25,29 +29,50 @@ def parse(url_params):
         if int(p_value) != page:
             break
         try:
-            elements = driver.find_elements('class name', '_93444fe79c--container--Povoi')[::]
-        except:
+            elements = driver.find_elements(
+                'class name', '_93444fe79c--container--Povoi'
+            )[::]
+        except Exception:
             return
-        for element in elements:
+
+        urls = [
+            element.find_element(
+                'class name', '_93444fe79c--media--9P6wN'
+            ).get_attribute('href') for element in elements
+        ]
+            
+        for url in urls:
             try:
-                url_new = element.find_element('class name', '_93444fe79c--media--9P6wN').get_attribute('href')
-                driver.get(url_new)
-                price = int(driver.find_element('css selector', "[data-name=PriceInfo]").find_element('class name', 'a10a3f92e9--color_black_100--Ephi7').text[:-2].replace(' ', ''))
-                text = driver.find_element('class name', 'a10a3f92e9--layout--BaqYw').find_element('class name', 'a10a3f92e9--color_black_100--Ephi7').text
+                driver.get(url)
+
+                price = int(driver.find_element(
+                    'css selector', "[data-name=PriceInfo]"
+                ).find_element(
+                    'class name', 'a10a3f92e9--color_black_100--Ephi7'
+                ).text[:-2].replace(' ', ''))
+
+                text = driver.find_element(
+                    'class name', 'a10a3f92e9--layout--BaqYw'
+                ).find_element(
+                    'class name', 'a10a3f92e9--color_black_100--Ephi7'
+                ).text
+
                 floor = ...
+
                 floors = ...
+
                 insert_data(...)
-            except:
-                ...
+            except Exception:
+                continue
 
 
 regions = {
-    'msc': '1', # москва
-    'spb': '2', # санкт-петербург
-    'ekb': '4743', # екатеринбург
-    'nsk': '4897', # новосибирск
-    'kzn': '4777', # казань
-    'nng': '4885', # нижний новгород
+    'msc': '1',  # москва
+    'spb': '2',  # санкт-петербург
+    'ekb': '4743',  # екатеринбург
+    'nsk': '4897',  # новосибирск
+    'kzn': '4777',  # казань
+    'nng': '4885',  # нижний новгород
 }
 
 for reg in regions.values():
