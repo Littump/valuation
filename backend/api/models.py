10188c386c511fab4a31c78e8003950249d8e5da
Roman Yakimov <littump@yandex.ru>
2023-11-01 01:03:10 +0300

    rewrite models

--- backend/api/models.py
+++ backend/api/models.py
@@ -10,25 +10,67 @@ class User(AbstractUser):
         ('employee', 'Сотрудник'),
     )
 
-    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+    role = models.CharField(choices=ROLE_CHOICES)
 
 
 class Property(models.Model):
-    address = models.TextField()
-    house_material = models.TextField()
-    object_type = models.CharField(max_length=10)
+
+    HOUSE_MATERIAL_CHOICES = (
+        ('brc', 'Кирпичный'),
+        ('mnl', 'Монолитный'),
+        ('pnl', 'Панельный'),
+        ('blc', 'Блочный'),
+        ('wdn', 'Деревянный'),
+        ('stl', 'Сталинский'),
+        ('brm', 'Кирпично-монолитный'),
+    )
+    house_material = role = models.CharField(choices=HOUSE_MATERIAL_CHOICES)
+
+    OBJECT_TYPE = (
+        (1, 'Первичка'),
+        (2, 'Вторичка'),
+    )
+    object_type = models.PositiveIntegerField(choices=OBJECT_TYPE)
+
+    REPAIR_TYPE = (
+        ...
+    )
+    repair = models.CharField(choices=REPAIR_TYPE)
+
+    METRO_HOW_TYPE = (
+        (1, 'Пешком'),
+        (2, 'На машине'),
+    )
+    metro_how = models.PositiveIntegerField(choices=METRO_HOW_TYPE)
+
+    HAS_LIFT_TYPE = (
+        (1, 'Имеется'),
+        (2, 'Отсутствует'),
+    )
+    has_lift = models.PositiveIntegerField(choices=HAS_LIFT_TYPE)
+
+    PARKING_TYPE_TYPE = (
+        ('grn', 'Наземная'),
+        ('mlt', 'Многоуровневая'),
+        ('und', 'Подземная'),
+        ('orf', 'На крыше'),
+        ('none', 'Отсутствует'),
+    )
+    parking_type = models.CharField(choices=PARKING_TYPE_TYPE)
+
+    address = models.CharField()
+    region = models.CharField()
+    metro_name = models.CharField()
+
+    floor = models.IntegerField()
     cnt_rooms = models.PositiveIntegerField()
-    floor = models.PositiveIntegerField()
-    area = models.FloatField()
-    repair = models.CharField(max_length=10)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
     price = models.PositiveIntegerField()
-    region = models.CharField(max_length=255)
     floors = models.PositiveIntegerField()
     house_year = models.PositiveIntegerField()
-    metro_name = models.CharField(max_length=255)
     metro_min = models.PositiveIntegerField()
-    metro_how = models.CharField(max_length=255)
+
+    area = models.FloatField()
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
     text = models.TextField(blank=True)
-    has_lift = models.CharField(max_length=255, blank=True)
-    parking_type = models.CharField(max_length=255, blank=True)
