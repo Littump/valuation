f059b6cc4a50e0fc2f1f68e22f2c0b48883f8a35
дюша <113046245+p0ntific@users.noreply.github.com>
2023-11-08 20:46:21 +0300

    Update getBuildingsHook.js

--- frontend/src/hooks/getBuildingsHook.js
+++ frontend/src/hooks/getBuildingsHook.js
@@ -2,5 +2,5 @@ import { useQuery } from "react-query";
 import userBuildingsService from "../services/userBuildingsService";
 
 export const getBuildingsHook = () => {
-  return useQuery({queryKey["getBuildings"],queryFn: () => userBuildingsService.getBuildings(),retry:true });
+  return useQuery({queryKey:["getBuildings"],queryFn: () => userBuildingsService.getBuildings(),retry:true });
 };
