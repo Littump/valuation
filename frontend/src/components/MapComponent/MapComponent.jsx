b9f3f1c0fcd0764047f2eae101af1c383ed3e520
дюша <113046245+p0ntific@users.noreply.github.com>
2023-11-09 10:30:02 +0300

    Update MapComponent.jsx

--- frontend/src/components/MapComponent/MapComponent.jsx
+++ frontend/src/components/MapComponent/MapComponent.jsx
@@ -6,6 +6,7 @@ export default function MapComponent() {
   let houseCoordinates = useSelector(
     (state) => state.building.coordinates //coordinates
   );
+  console.log(houseCoordinates)
   let similarBuildings = useSelector(
     (state) => state.similarBuildings.buildings
   );
